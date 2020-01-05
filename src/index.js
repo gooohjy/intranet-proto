260e2d1268a769fcb0a8c528283f56d7ebbb4513
gooohjy <36816180+gooohjy@users.noreply.github.com>
2020-01-05 16:42:24 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,7 +6,26 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+///// From the other repo
+/**
+ * Module dependencies.
+ */
+var Strategy = require('./strategy');
+
+
+/**
+ * Framework version.
+ */
+require('pkginfo')(module, 'version');
+
+/**
+ * Expose constructors.
+ */
+exports.Strategy = Strategy;
+/////
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
